e3bb6b1e32e949bd55c5724d3eb607b1095fbd40
Alvaro Videla <videlalvaro@gmail.com>
2020-11-17 14:55:36 +0100

    comments if clauses

--- bin/serverless.php
+++ bin/serverless.php
@@ -1,14 +1,14 @@
 <?php
 declare(strict_types=1);
 
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+if (file_exists(__DIR__ . '/vendor/autoload.php')) { // the script is being run on the project's main folder
     require_once __DIR__ . '/vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../autoload.php')) {
+} elseif (file_exists(__DIR__ . '/../autoload.php')) { // the script is being run out of the vendor/bin folder
     /** @noinspection PhpIncludeInspection */
     require_once __DIR__ . '/../autoload.php';
 } else {
     /** @noinspection PhpIncludeInspection */
-    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../vendor/autoload.php'; // the script is being run in the project's bin folder
 }
 
 use Azserverless\Runtime\Router;
