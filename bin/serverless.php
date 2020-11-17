04a88fd94399103e75034c55f91bf14ef09841d8
Alvaro Videla <videlalvaro@gmail.com>
2020-11-17 15:51:43 +0100

    fixes __DIR__ symlink problem

--- bin/serverless.php
+++ bin/serverless.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 if (file_exists(__DIR__ . '/vendor/autoload.php')) { // the script is being run on the project's main folder
     require_once __DIR__ . '/vendor/autoload.php';
-} elseif (file_exists(__DIR__ . '/../autoload.php')) { // the script is being run out of the vendor/bin folder
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) { // the script is being run out of the vendor/videlalvaro/azserverless/bin folder
     /** @noinspection PhpIncludeInspection */
-    require_once __DIR__ . '/../autoload.php';
+    require_once __DIR__ . '/../../../autoload.php';
 } else {
     /** @noinspection PhpIncludeInspection */
     require_once __DIR__ . '/../vendor/autoload.php'; // the script is being run in the project's bin folder
